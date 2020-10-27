8dda9e9c177eee48a3eacf9cfa5a5c690979217b
Léo Salé <leo.sale72@gmail.com>
2020-10-27 20:50:09 +0100

    add spectators model and controller

---
+++ controllers/spectator.js
@@ -0,0 +1,30 @@
+const db = require('../models/index');
+const Spectator = db.Spectator;
+
+// Create and save a new Spectator
+exports.create = async (req, res) => {
+  try {
+    const spectator = {
+      ticketNumber: req.body.ticketNumber,
+      vote: req.body.vote,
+    };
+
+    await Spectator.create(spectator);
+
+    res.status(201).send({ message: 'Spectator was successfully created' });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ message: err.message });
+  }
+};
+
+// Retrieve all Spectators
+exports.findAll = async (req, res) => {
+  try {
+    const spectators = await Spectator.findAll();
+
+    res.status(200).send(spectators);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
